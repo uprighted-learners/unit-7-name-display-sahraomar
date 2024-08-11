62ae9c8b1e6050f132d63b96997df39df2c5879e
sahraomar <sahraaliwrites@gmail.com>
2024-08-11 07:55:42 -0400

    assignment complete-commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
-import './App.css';
-
+import { useState } from 'react';
 function App() {
-  return (
-    <>App</>
+  const message = "Hi there! Please enter your name";
+ //state variables
+const [firstName, setFirstName] = useState("");
+const [middleName, setMiddleName] = useState("");
+const [lastName, setLastName] = useState("");
+ //combine the names 
+ const fullName = `${firstName} ${middleName} ${lastName}`.trim();
+ 
+ return (
+    <div>
+    <h1>{message}</h1>
+<form>
+  {/*First Name input */}
+<input type="text" placeholder="First Name"
+onChange={(e) =>  setFirstName(e.target.value)}
+/>
+  {/*Middle Name input */}
+
+<input 
+type="text"
+placeholder="Middle Name"
+onChange={(e)=> setMiddleName(e.target.value)}
+/>
+  {/*Last Name input */}
+
+<input 
+type="text"
+placeholder="Last Name"
+onChange={(e)=> setLastName(e.target.value)}
+/>
+
+</form>
+
+{/*display the full name */}
+
+ {/* Display the full name */}
+ <h2>Hello, {fullName}!</h2>
+    </div>
   );
 }
 
